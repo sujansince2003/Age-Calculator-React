9c873ee5a2323c78bab53f474fe72209c4dbbcc3
sujan khatri <sujansince2059@gmail.com>
2023-09-18 12:54:05 +0545

    creating age calcualtor

---
+++ agecalculator/src/App.jsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="calbox">
+          <div className="inputbox">
+            <form>
+              <div className="inputbox">
+                day
+                <input type="text" />
+              </div>
+              <div className="inputbox">
+                Month
+                <input type="text" />
+              </div>
+              <div className="inputbox">
+                Year
+                <input type="text" />
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default App;
