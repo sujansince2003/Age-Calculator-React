a5593f29458ab94c99a5ca00aca5961dbd42ba3c
sujan khatri <sujansince2059@gmail.com>
2023-09-20 21:19:44 +0545

    sc

--- agecalculator/src/App.jsx
+++ agecalculator/src/App.jsx
@@ -2,32 +2,36 @@ import { useState } from "react";
 
 function App() {
   const date = new Date();
-  let agedate;
-  console.log(date.getDate());
 
   const [flag, setFlag] = useState(0);
-  const [years, setYears] = useState(0);
-  const [days, setDays] = useState(0);
-  const [months, setMonths] = useState(0);
-  const [age, setAge] = useState({});
+  const [years, setYears] = useState("");
+  const [days, setDays] = useState("");
+  const [months, setMonths] = useState("");
+  const [age, setAge] = useState({ year: 0 });
+
   function setdata() {
-    setFlag(1);
     if (
-      (months && days && years && isNaN(months)) ||
-      isNaN(days) ||
-      isNaN(years) ||
-      months < 1 ||
-      months > 12 ||
-      days < 1 ||
-      days > 30
-    )
+      !isNaN(days) &&
+      !isNaN(months) &&
+      !isNaN(years) &&
+      months >= 1 &&
+      months <= 12 &&
+      days >= 1 &&
+      days <= 31
+    ) {
+      setFlag(1);
       setDays((days) => parseInt(days));
-    setMonths((months) => parseInt(months));
-    setYears((years) => parseInt(years));
-    agedate = new Date("years,months,date");
-    setAge({
-      year: date.getFullYear() - agedate.getFullYear(),
-    });
+      setMonths((months) => parseInt(months));
+      setYears((years) => parseInt(years));
+      const agedate = new Date(`${years}-${months}-${days}`);
+      setAge({
+        year: date.getFullYear() - agedate.getFullYear(),
+        month: date.getMonth() - agedate.getMonth(),
+        day: date.getDate() - agedate.getDate(),
+      });
+    } else {
+      setFlag(2);
+    }
   }
 
   return (
@@ -36,6 +40,23 @@ function App() {
       <div className="container">
         <div className="calbox">
           <div className="inputbox">
+            <div className="inputbox2">
+              Year
+              <input
+                type="text"
+                value={years}
+                onChange={(e) => setYears(e.target.value)}
+              />
+            </div>
+
+            <div className="inputbox2">
+              Month
+              <input
+                type="text"
+                value={months}
+                onChange={(e) => setMonths(e.target.value)}
+              />
+            </div>
             <div className="inputbox2">
               day
               <input
@@ -46,33 +67,35 @@ function App() {
                 }}
               />
             </div>
-            <div className="inputbox2">
-              Month
-              <input type="text" onChange={(e) => setMonths(e.target.value)} />
-            </div>
-            <div className="inputbox2">
-              Year
-              <input type="text" onChange={(e) => setYears(e.target.value)} />
-            </div>
           </div>
           <div className="btn">
             <button onClick={setdata}>Calculate</button>
-            <div>ff</div>
           </div>
-          <div className="results">
-            <span>
-              <span className="valuee">{flag == 1 ? age.year : " "}</span> Years
-            </span>
-            <span>
-              <span className="valuee">{flag == 1 ? months : " "}</span> Months
-            </span>
-            <span>
-              <span className="valuee">{flag == 1 ? days : " "}</span> Days
-            </span>
-          </div>
+          {flag === 1 ? (
+            <div className="results">
+              <span>
+                <span className="valuee">{age.year}</span> Years
+              </span>
+              <span>
+                <span className="valuee">{age.month}</span> Months
+              </span>
+              <span>
+                <span className="valuee">{age.day}</span> Days
+              </span>
+            </div>
+          ) : flag != 2 ? (
+            <>
+              <div className="message">ENTER THE DATE</div>
+            </>
+          ) : (
+            <>
+              <div className="message">PROVIDED DATA IS NOT ACCEPTED</div>
+            </>
+          )}
         </div>
       </div>
     </>
   );
 }
+
 export default App;
